95cbdb011d30356b3756ac292e01f178381f4c42
robadon32 <rob.adon32@gmail.com>
2020-07-22 01:17:06 -0400

    Version 3.0

--- app/src/main/java/com/example/robad/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/robad/scorekeeper/MainActivity.java
@@ -3,16 +3,29 @@ package com.example.robad.scorekeeper;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String KEY_MY_GUEST_SCORE = "KEY_GUEST_SCORE";
+    private static final String KEY_MY_HOME_SCORE = "KEY_HOME_SCORE";
+    private static final String KEY_MY_CURRENT_INNING = "KEY_CURRENT_INNING";
+    private static final String KEY_MY_STRIKE_COUNT = "KEY_STRIKE_COUNT";
+    private static final String KEY_MY_BALL_COUNT = "KEY_BALL_COUNT";
+    private static final String KEY_MY_OUT_COUNT = "KEY_OUT_COUNT";
+
     int guestScore, homeScore, ballCount, strikeCount, outCount, inningCount;
+    String strike = "";
+    String ball = "";
+    String out = "";
     boolean isGuestTurn = true;
     TextView guestView, homeView;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +36,35 @@ public class MainActivity extends AppCompatActivity {
         guestView = findViewById(R.id.guest);
         homeView = findViewById(R.id.home);
         guestView.setTextColor(ColorStateList.valueOf(getResources().getColor(R.color.colorAccent)));
+    }
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "inside of onSaveInstanceState");
+        outState.putInt(KEY_MY_GUEST_SCORE, guestScore);
+        outState.putInt(KEY_MY_HOME_SCORE, homeScore);
+        outState.putInt(KEY_MY_CURRENT_INNING, inningCount);
+        outState.putString(KEY_MY_STRIKE_COUNT, strike);
+        outState.putString(KEY_MY_BALL_COUNT, ball);
+        outState.putString(KEY_MY_OUT_COUNT, out);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "inside of onRestoreInstanceState");
+        guestScore = savedInstanceState.getInt(KEY_MY_GUEST_SCORE, 0);
+        homeScore = savedInstanceState.getInt(KEY_MY_HOME_SCORE, 0);
+        inningCount = savedInstanceState.getInt(KEY_MY_CURRENT_INNING, 0);
+        strike = savedInstanceState.getString(KEY_MY_STRIKE_COUNT, "");
+        ball = savedInstanceState.getString(KEY_MY_BALL_COUNT, "");
+        out = savedInstanceState.getString(KEY_MY_OUT_COUNT, "");
+        displayGuestScore(guestScore);
+        displayHomeScore(homeScore);
+        displayInning(inningCount);
+        displayStrikeCount(strike);
+        displayBallCount(ball);
+        displayOutCount(out);
     }
     /**
      * Increases the Guest Score by 1. Checks for a winner once inning reaches 9 or more.
@@ -44,19 +86,23 @@ public class MainActivity extends AppCompatActivity {
     public void ballCount(View v){
         ballCount = ballCount + 1;
         if (ballCount == 1){
-            displayBallCount("B");
+            ball = ball + "B";
+            displayBallCount(ball);
         }
         if (ballCount == 2){
-            displayBallCount("BB");
+            ball = ball + "B";
+            displayBallCount(ball);
         }
         if (ballCount == 3){
-            displayBallCount("BBB");
+            ball = ball + "B";
+            displayBallCount(ball);
         }
         if (ballCount == 4){
             displayBallCount("-----");
             displayStrikeCount("-----");
+            ball = "";
             ballCount = 0;
-            strikeCount = 1;
+            strikeCount = 0;
         }
     }
     /**
@@ -66,26 +112,35 @@ public class MainActivity extends AppCompatActivity {
     public void strikeCount(View v){
         strikeCount = strikeCount + 1;
         if (strikeCount == 1){
-            displayStrikeCount("S");
+            strike = strike + "S";
+            displayStrikeCount(strike);
         } else if (strikeCount == 2){
-            displayStrikeCount("SS");
+            strike = strike + "S";
+            displayStrikeCount(strike);
         } else {
             outCount = outCount + 1;
             displayBallCount("-----");
             displayStrikeCount("-----");
+            strike = "";
+            ball = "";
             strikeCount = 0;
-            ballCount = 1;
+            ballCount = 0;
             if (outCount == 1) {
-                displayOutCount("O");
+                out = out + "O";
+                displayOutCount(out);
             } else if(outCount == 2){
-                displayOutCount("OO");
+                out = out + "O";
+                displayOutCount(out);
             } else {
                 displayOutCount("-----");
                 displayBallCount("-----");
                 displayStrikeCount("-----");
-                outCount = 0;
-                ballCount = 1;
+                strike = "";
+                ball = "";
+                out = "";
                 strikeCount = 0;
+                ballCount = 0;
+                outCount = 0;
                 isGuestTurn = !isGuestTurn;
             }
         }
@@ -255,6 +310,9 @@ public class MainActivity extends AppCompatActivity {
         ballCount = 0;
         strikeCount = 0;
         inningCount = 1 ;
+        strike = "";
+        ball = "";
+        out = "";
         guestView.setTextColor(ColorStateList.valueOf(getResources().getColor(R.color.colorAccent)));
         homeView.setTextColor(ColorStateList.valueOf(getResources().getColor(R.color.topOfInning)));
     }
--- app/src/main/java/com/example/robad/scorekeeper/WinnerActivity.java
+++ app/src/main/java/com/example/robad/scorekeeper/WinnerActivity.java
@@ -1,24 +1,113 @@
 package com.example.robad.scorekeeper;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class WinnerActivity extends AppCompatActivity {
+public class WinnerActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView winnerView;
+    public static final String TAG = "MainActivity";
+    private ImageView phoneView, messageView, mapView;
+    String theWinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_winner);
+        Log.d(TAG, "Start of onCreate method");
 
-        winnerView = findViewById(R.id.theWinner);
+        getReferenceToViews();
+        setListeners();
 
         Bundle extras = getIntent().getExtras();
         if (extras == null) {
             return;
         }
-        String theWinner = extras.getString("winner");
+        theWinner = extras.getString("winner");
+        TextView winnerView = findViewById(R.id.theWinner);
         winnerView.setText(theWinner);
+
+        Log.d(TAG, "end of onCreate method");
+    }
+
+    private void getReferenceToViews() {
+        phoneView = findViewById(R.id.imageView_call);
+        messageView = findViewById(R.id.imageView_text);
+        mapView = findViewById(R.id.imageView_map);
+    }
+
+    private void setListeners() {
+        Log.d(TAG, "Start of setListeners method");
+        phoneView.setOnClickListener(this);
+        messageView.setOnClickListener(this);
+        mapView.setOnClickListener(this);
+        Log.d(TAG, "End of setListeners method");
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Log.d(TAG, "Start of onClick method");
+
+        switch (v.getId()) {
+            case R.id.imageView_call:
+                Log.d(TAG, "Call button was clicked");
+                makePhoneCall();
+                break;
+            case R.id.imageView_text:
+                Log.d(TAG, "Text button was clicked");
+                sendText();
+                break;
+            case R.id.imageView_map:
+                Log.d(TAG, "Map button was clicked");
+                findLocation();
+                break;
+        }
+
+        Log.d(TAG, "End of onClick method");
+
+    }
+
+    private void findLocation() {
+        Log.d(TAG, "Start of findLocation method");
+        String loc = "baseball field near me";
+        Uri geoLoc = Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, geoLoc);
+        if(intent.resolveActivity(getPackageManager())   !=  null  ){
+            startActivity(intent);
+        }
+        else{
+            Log.d(TAG, "Cannot find location " + loc);
+            Toast.makeText(this, "Cannot find location " + loc, Toast.LENGTH_LONG).show();
+        }
+        Log.d(TAG, "End of findLocation method");
+    }
+
+    private void sendText() {
+        Log.d(TAG, "Start of sendText method");
+        String mimeType = "text/plain";
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setType(mimeType)
+                .setChooserTitle("Pick an app")
+                .setText(theWinner + " We are the CHAMPIONS! Let's pop some champagne.")
+                .startChooser();
+        Log.d(TAG, "End of sendText method");
+
+    }
+
+    private void makePhoneCall() {
+        Log.d(TAG, "Start of makePhoneCall method");
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        startActivity(intent);
+        Log.d(TAG, "End of makePhoneCall method");
     }
 }
